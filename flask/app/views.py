b57040f28557eb34f940e951701057de9fec27a3
Oleh Kyslashko <nuxkill@gmail.com>
2019-04-08 02:43:45 +0300

    add json get example and json previw template

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,5 +1,17 @@
 from app import app
+from flask import jsonify, render_template
+import json
+import requests
+## https://min-api.cryptocompare.com/data/histominute?fsym=BTC&tsym=USD&limit=30
 
 @app.route("/")
 def index():
-    return "aasdfasdf122312sdfasFlask dfasddf"
+    url =  'https://min-api.cryptocompare.com/data/histominute?fsym=BTC&tsym=USD&limit=30'
+    res = requests.get(url)
+    data = res.json()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return render_template('preview.html', data=json.dumps(data))
