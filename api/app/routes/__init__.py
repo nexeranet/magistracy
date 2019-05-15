4a03c694e42cbebc1abce2e17d9a0cb521943d82
Oleh Kyslashko <nuxkill@gmail.com>
2019-05-15 20:53:06 +0300

    add tree

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,6 +1,8 @@
-from .views import views 
+from .views import views
 from .db import apidb
 from .api import api
+
+
 def init_app(app):
     app.register_blueprint(views)
     app.register_blueprint(apidb, url_prefix='/db')
