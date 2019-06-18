3652ba6bf5de63700dfed88c9aae188881041b8e
Oleh Kyslashko <nuxkill@gmail.com>
2019-06-18 14:47:24 -0400

    add 404

--- api/app/routes/__init__.py
+++ api/app/routes/__init__.py
@@ -1,8 +1,14 @@
 from .views import views
 from .db import apidb
 from .api import api
+from flask import redirect
 
 def init_app(app):
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return redirect('/', 302)
+
     app.register_blueprint(views)
     app.register_blueprint(apidb, url_prefix='/db')
     app.register_blueprint(api, url_prefix='/api')
